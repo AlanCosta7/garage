b0cd280de6837bd5ee07ffa7988652c88faff6d0
Leonardo Merlin <leonardo.merlin@gmail.com>
2018-09-08 14:50:55 -0300

    add: meta tags e ajusta a tela de login

--- src/pages/user/login.vue
+++ src/pages/user/login.vue
@@ -2,9 +2,8 @@
     <q-page>
        <q-layout class="flex items-center column"> 
             <h2 style="font-weight: 50">Seja bem vindo!</h2>
-            <h5 style="font-weight: 300">aqui é seu ambiente criativo</h5>
-                <q-card class="login">login</q-card>
-                <div id="firebaseui-auth-container"></div>
+            <h5 style="font-weight: 300">Acesse agora seu ambiente criativo</h5>
+            <div id="firebaseui-auth-container"></div>
       </q-layout>
     </q-page>
 </template>
@@ -14,95 +13,52 @@ import * as firebase from 'firebase'
 import firebaseui from 'firebaseui'
 
 export default {
-    name: 'PageLogin',
-        data () {
-            return {  
-            }
-        },
-    mounted() {
-        var uiConfig = {
-            signInSuccessUrl: '/',
-            signInOptions: [
-                firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-                firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-                firebase.auth.EmailAuthProvider.PROVIDER_ID,
-                ]
-            };
-            var ui = new firebaseui.auth.AuthUI(firebase.auth());
-            ui.start('#firebaseui-auth-container', uiConfig);
-            },
-    }  
+  name: 'PageLogin',
+  meta: { title: 'garage thinking - login' },
+  data() {
+    return {}
+  },
+  mounted() {
+    var uiConfig = {
+      signInSuccessUrl: '/',
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+      ],
+      callbacks: {
+        uiShown: function() {
+          // The widget is rendered.
+
+          // Update text content of buttons
+          document.querySelector('.firebaseui-idp-google .firebaseui-idp-text-long').textContent = 'Acessar com Google'
+          document.querySelector('.firebaseui-idp-facebook .firebaseui-idp-text-long').textContent =
+            'Acessar com Facebook'
+          document.querySelector('.firebaseui-idp-password .firebaseui-idp-text-long').textContent = 'Acessar com Email'
+        }
+      }
+    }
+    var ui = new firebaseui.auth.AuthUI(firebase.auth())
+    ui.start('#firebaseui-auth-container', uiConfig)
+  }
+}
 </script>
 
 <style>
-    .firebaseui-list-item {
-        position: relative;
-        list-style: none;
-        margin: 10px
+.firebaseui-idp-button {
+  max-width: 300px;
+  min-height: 60px;
+}
 
-    }
-    .firebaseui-idp-icon {
-        width: 100px;
-        height: 30px;
-    }
-    .firebaseui-idp-text {
-        display:none
-    }
-    .firebaseui-idp-button {
-        background-color: transparent;
-        border: none;
-    }
-    .firebaseui-idp-google {
-        background-color: rgb(228, 225, 225);
-        height: 60px;
-        width: 100%;
-        max-width: 300px;
-        border-radius: 10px;
-        border: 2px;
-        border-color: black
-    }
-    .firebaseui-idp-facebook {
-        background-color: steelblue;
-        height: 60px;
-        border-radius: 10px;
-        width: 100%;
-        max-width: 300px;
-    }
-    #firebaseui-auth-container {
-        text-align: center;
-        width: 100%;
-    }
-    .firebaseui-idp-password {
-        background-color: crimson;
-        height: 60px;
-        border-radius: 10px;
-        width: 100%;
-        max-width: 300px;
-    }
-    .firebaseui-idp-list {
-        display: inline;
-    }
-    .firebaseui-title {
-        display: none
-    }
-    .firebaseui-info-bar-message {
-        display: none        
-    }
-    ul {
-        margin: 0;
-        padding: 0
-    }
-    h5 {
-        padding: 0;
-        margin: 0
-    }
-    .login {
-        margin-top: 10%;
-        margin-bottom: 1%;
-        padding: 1%;
-        width: 300px;
-        text-align: center;
-        background-color: orange;
-        font-weight: bold
-    }
+.firebaseui-idp-text {
+  font-size: 18px;
+}
+ul {
+  margin: 0;
+  padding: 0;
+}
+h5 {
+  padding: 0;
+  margin: 0;
+}
 </style>
